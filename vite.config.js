d8606872c31e44a5f9351ca23db9815dcb2817ea
ConstantMotion1 <constan.fernando@hotmail.com>
2023-10-26 17:07:24 -0400

    build dev site

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,28 @@
-const path = require('path')
+// const path = require('path')
+
+// export default {
+//   root: path.resolve(__dirname, 'src'),
+//   resolve: {
+//     alias: {
+//       '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+//     }
+//   },
+//   server: {
+//     port: 8080,
+//     hot: true
+//   }
+// }
+
+const path = require('path');
 
 export default {
-  root: path.resolve(__dirname, 'src'),
+  base: '/simplesite/', // Change this to match your project name
   resolve: {
     alias: {
-      '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+      '~/bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'), // Adjust the alias path if necessary
     }
   },
   server: {
-    port: 8080,
-    hot: true
-  }
-}
+    port: 8080, // You can keep this for local development
+  },
+};
